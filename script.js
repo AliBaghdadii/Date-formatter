5cbf352bb8d5388d0bd6264a52d293a6629001d9
Ali Baghdadi <abgr8gr8@gmail.com>
2025-03-07 20:32:24 +0330

    Step 25

--- script.js
+++ script.js
@@ -16,5 +16,9 @@ dateOptionsSelectElement.addEventListener("change", () => {
         case "yyyy-mm-dd":
             currentDateParagraph.textContent = formattedDate.split("-").reverse().join("-");
             break;
+            
+        case "mm-dd-yyyy-h-mm":
+            currentDateParagraph.textContent = ``;
+            break;
     }
 });
